9b591e566ddae33b19e2a9264b107a5e8dad8f25
Mromoc1 <mromo@alumnosuls.cl>
2020-02-07 21:41:58 -0400

    Union BD - ASP sin estructura (sin MVC ni capas)

---
+++ Practica1/WebForm1.aspx
@@ -0,0 +1,46 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Practica1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <table>
+                <tr>
+                    <td>Nombre</td>
+                    <td><asp:TextBox ID="Nombre" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Apellido Paterno</td>
+                    <td><asp:TextBox ID="ApellidoP" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td>Apellido Materno</td>
+                    <td><asp:TextBox ID="ApellidoM" runat="server"></asp:TextBox></td>
+                </tr>
+
+                <tr>
+                    <td colspan="2" align="center">
+                        <asp:Button ID="Button1" runat="server" Text="Insertar" OnClick="Button1_Click" />
+                        <asp:Button ID="Button2" runat="server" Text="Borrar" />
+                        <asp:Button ID="Button3" runat="server" Text="Actualizar" />
+                        <asp:Button ID="Button4" runat="server" Text="Ver" />
+                    </td>
+                </tr>
+            </table>
+            <br />
+            <asp:GridView ID="GridView1" runat="server" Width="364px"></asp:GridView>
+
+
+        </div>
+    </form>
+</body>
+</html>
---
+++ Practica1/WebForm1.aspx.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Practica1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        SqlConnection conexion = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\Practica1BD.mdf;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (conexion.State == ConnectionState.Open )
+            {
+                conexion.Close();
+            }
+            conexion.Open();
+            Mostrar_BD();
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = conexion.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "insert into Tablapersona values ('" + Nombre.Text+"','"+ApellidoP.Text+"','"+ApellidoM.Text+"')";
+            cmd.ExecuteNonQuery();
+
+            Nombre.Text = "";
+            ApellidoM.Text = "";
+            ApellidoP.Text = "";
+            Mostrar_BD();
+        }
+
+        public void Mostrar_BD()
+        {
+            SqlCommand cmd = conexion.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from Tablapersona";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        } 
+    }
+}
